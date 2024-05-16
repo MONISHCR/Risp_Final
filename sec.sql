d009130b3929f80a0d61c708db00665f7d859d84
B.MONISH <111340243+MONISHCR@users.noreply.github.com>
2024-05-16 13:51:11 +0530

    Update sec.sql

--- sec.sql
+++ sec.sql
@@ -29,9 +29,18 @@ VALUES
 
 INSERT INTO timetable (class, section, day, period1, period2, period3, lunch, period4, period5, period6)
 VALUES
-(7, 'B', 'Monday', 'Maths', 'Science', 'Social', 'Telugu', 'Hindi', 'English', ''),
-(7, 'B', 'Tuesday', 'Science', 'Social', 'Telugu', 'Hindi', 'English', 'Maths', ''),
-(7, 'B', 'Wednesday', 'Social', 'Telugu', 'Hindi', 'English', 'Maths', 'Science', ''),
-(7, 'B', 'Thursday', 'Telugu', 'Hindi', 'English', 'Maths', 'Science', 'Social', ''),
-(7, 'B', 'Friday', 'Hindi', 'English', 'Maths', 'Science', 'Social', 'Telugu', ''),
-(7, 'B', 'Saturday', 'English', 'Maths', 'Science', 'Social', 'Telugu', 'Hindi', '');
+(7, 'B', 'Monday', 'Maths', 'Science', 'Social','LB', 'Telugu', 'Hindi', 'English'),
+(7, 'B', 'Tuesday', 'Science', 'Social', 'Telugu','UR', 'Hindi', 'English', 'Maths'),
+(7, 'B', 'Wednesday', 'Social', 'Telugu', 'Hindi', 'NE','English', 'Maths', 'Science'),
+(7, 'B', 'Thursday', 'Telugu', 'Hindi', 'English', 'CA','Maths', 'Science', 'Social'),
+(7, 'B', 'Friday', 'Hindi', 'English', 'Maths','LB', 'HK', 'Social', 'Telugu'),
+(7, 'B', 'Saturday', 'English', 'Maths', 'Science', '30MIN','Social', 'Telugu', 'Hindi');
+
+INSERT INTO timetable (class, section, day, period1, period2, period3, lunch, period4, period5, period6)
+VALUES
+(8, 'A', 'Monday', 'Science', 'Social', 'Telugu', 'LB','Hindi', 'English', 'Maths'),
+(8, 'A', 'Tuesday', 'Social', 'Telugu', 'Hindi', 'UR','English', 'Maths', 'Science'),
+(8, 'A', 'Wednesday', 'Telugu', 'Hindi', 'English', 'NE','Maths', 'Science', 'Social'),
+(8, 'A', 'Thursday', 'Hindi', 'English', 'Maths', 'CA','Science', 'Social', 'Telugu'),
+(8, 'A', 'Friday', 'English', 'Maths', 'Science', 'HK','Social', 'Telugu', 'Hindi'),
+(8, 'A', 'Saturday', 'Maths', 'Science', 'Social', '30MIN','Telugu', 'Hindi', 'English');
