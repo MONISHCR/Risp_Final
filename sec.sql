6a1bdde4aff387e29b05f4e905da94788ac78740
B.MONISH <111340243+MONISHCR@users.noreply.github.com>
2024-05-16 13:13:20 +0530

    Create sec.sql

---
+++ sec.sql
@@ -0,0 +1,28 @@
+INSERT INTO timetable (class, section, day, period1, period2, period3, lunch, period4, period5, period6)
+VALUES
+(6, 'A', 'Monday', 'Hindi', 'English', 'Maths', 'L B', 'Science', 'Social', 'Telugu'),
+(6, 'A', 'Tuesday', 'English', 'Maths', 'Hindi', 'U R', 'Science', 'Social', 'Telugu'),
+(6, 'A', 'Wednesday', 'Maths', 'Science', 'Social', 'N E', 'Telugu', 'Hindi', 'English'),
+(6, 'A', 'Thursday', 'Science', 'Social', 'Telugu', 'C A', 'Hindi', 'English', 'Maths'),
+(6, 'A', 'Friday', 'Social', 'Telugu', 'Hindi', 'H K', 'English', 'Maths', 'Science'),
+(6, 'A', 'Saturday', 'Telugu', 'Hindi', 'English', '30MIN', 'Maths', 'Science', 'Social');
+
+
+INSERT INTO timetable (class, section, day, period1, period2, period3, lunch, period4, period5, period6)
+VALUES
+(6, 'B', 'Monday', 'Telugu', 'Hindi', 'English', 'LB','Maths', 'Science', 'Social'),
+(6, 'B', 'Tuesday', 'Hindi', 'English', 'Maths','UR', 'Science', 'Social', 'Telugu'),
+(6, 'B', 'Wednesday', 'English', 'Maths', 'Science', 'NE','Social', 'Telugu', 'Hindi'),
+(6, 'B', 'Thursday', 'Maths', 'Science', 'Social', 'CA','Telugu', 'Hindi', 'English'),
+(6, 'B', 'Friday', 'Science', 'Social', 'Telugu', 'HK','Hindi', 'English', 'Maths'),
+(6, 'B', 'Saturday', 'Social', 'Telugu', 'Hindi', '30MIN','English', 'Maths', 'Science');
+
+
+INSERT INTO timetable (class, section, day, period1, period2, period3, lunch, period4, period5, period6)
+VALUES
+(7, 'A', 'Monday', 'English', 'Maths', 'Science',  'LB','Social', 'Telugu', 'Hindi'),
+(7, 'A', 'Tuesday', 'Maths', 'Science', 'Social',  'UR','Telugu', 'Hindi', 'English'),
+(7, 'A', 'Wednesday', 'Science', 'Social', 'Telugu', 'NE', 'Hindi', 'English', 'Maths'),
+(7, 'A', 'Thursday', 'Social', 'Telugu', 'Hindi', 'CA', 'English', 'Maths', 'Science'),
+(7, 'A', 'Friday', 'Telugu', 'Hindi', 'English',  'HK','Maths', 'Science', 'Social'),
+(7, 'A', 'Saturday', 'Hindi', 'English', 'Maths', '30MIN','Science', 'Social', 'Telugu');
